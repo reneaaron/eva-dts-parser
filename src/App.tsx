ad5ee4981cfd3b38f6ab8443153edc1e3a71deac
René Aaron <rene@twentyuno.net>
2024-04-23 17:40:07 +0200

    fix: initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button"
-import { useEffect } from "react"
+import { ChangeEvent, useState } from "react"
 import parseEvaDtsFile from "@/lib/parser";
 import { mkConfig, generateCsv, download } from "export-to-csv";
-import { config } from "process";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { Toaster } from "./components/ui/sonner";
+import { toast } from "sonner";
+import { LoadingButton } from "./components/loading-button";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
 
   const csvConfig = mkConfig({
     fieldSeparator: ',',
@@ -18,24 +24,49 @@ function App() {
     useKeysAsHeaders: true,
   });
 
-  useEffect(() => {
+  function handleFileChanged(event: ChangeEvent<HTMLInputElement>): void {
+    if (!event.target.files || !event.target.files[0]) return;
 
-    const parse = async () => {
-      const request = await fetch('/sample.txt');
-      const content = await request.text();
+    setFile(event.target.files[0]);
+  }
 
+  function submit() {
+    if(!file)
+        return;
+
+    setLoading(true);
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const content = e.target.result
       const result = parseEvaDtsFile(content);
-      console.log(result);
       const csv = generateCsv(csvConfig)(result);
       download(csvConfig)(csv);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+
+      toast("Konvertierung erfolgreich", {
+        description: "Die konvertierte Datei wird jetzt heruntergeladen."
+      })
     };
 
-    parse();
-  });
+    reader.readAsText(file); // Read file as text
+  }
 
   return (
-    <>
-      <Button>Click me</Button>
+    <>        
+      <Toaster />
+      <div className="flex flex-col justify-center items-center h-full gap-3">
+        <div className="w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="file">EVADTS Datei</Label>
+          <Input id="file" type="file" accept=".txt" onChange={handleFileChanged} />
+        </div>
+        <LoadingButton size="lg" loading={loading} onClick={submit} disabled={file === null}>
+          Start
+        </LoadingButton>
+      </div>
     </>
   )
 }
