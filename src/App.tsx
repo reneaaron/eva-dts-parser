89c7de5dcd2b10addd1c34efb340c462fa4ad5ff
René Aaron <rene@twentyuno.net>
2024-04-24 17:02:46 +0200

    feat: parse currency & total sales per payment method

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 import { ChangeEvent, useState } from "react"
-import parseEvaDtsFile from "@/lib/parser";
-import { mkConfig, generateCsv, download } from "export-to-csv";
+import parseEvaDtsFile, { ParseResult } from "@/lib/parser";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import { Toaster } from "./components/ui/sonner";
 import { toast } from "sonner";
 import { LoadingButton } from "./components/loading-button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table";
+import { Button } from "./components/ui/button";
+import { number } from "currency-codes";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState<File | null>(null);
-
-  const csvConfig = mkConfig({
-    fieldSeparator: ',',
-    quoteStrings: true,
-    decimalSeparator: '.',
-    title: 'Products',
-    useTextFile: false,
-    useBom: true,
-    columnHeaders: [
-      { key: "id", displayLabel: "Fach"},
-      { key: "price", displayLabel: "Einzelpreis" },
-      { key: "valueOfSales", displayLabel: "Umsatz" },
-      { key: "numberOfSales", displayLabel: "Anzahl Verkäufe" },
-      
-    ],
-  });
+  const [result, setResult] = useState<ParseResult | null>(null);
+  const [currency, setCurrency] = useState<string>("EUR");
 
   function handleFileChanged(event: ChangeEvent<HTMLInputElement>): void {
     if (!event.target.files || !event.target.files[0]) return;
@@ -34,43 +22,99 @@ function App() {
   }
 
   function submit() {
-    if(!file)
-        return;
+    if (!file)
+      return;
 
     setLoading(true);
     const reader = new FileReader();
 
     reader.onload = (e: ProgressEvent<FileReader>) => {
-      if(!e.target || !e.target.result) return;
+      if (!e.target || !e.target.result) return;
 
       const content = e.target.result as string;
       const result = parseEvaDtsFile(content);
-      const csv = generateCsv(csvConfig)(result as any);
-      download(csvConfig)(csv);
+      
+      const currentCurrency = number(result.currencyNumber as unknown as string);
+      if (currentCurrency) {
+        setCurrency(currentCurrency.code);
+      }
 
       setTimeout(() => {
+        toast("Datei erfolgreich verarbeitet.");
         setLoading(false);
+        setResult(result);
       }, 1000);
 
-      toast("Konvertierung erfolgreich", {
-        description: "Die konvertierte Datei wird jetzt heruntergeladen."
-      })
     };
 
     reader.readAsText(file); // Read file as text
   }
 
+  function formatCurrency(value: number) {
+    return new Intl.NumberFormat("de-AT", { style: "currency", currency: currency }).format(value);
+  }
+
   return (
-    <>        
+    <>
       <Toaster />
       <div className="flex flex-col justify-center items-center h-full gap-3">
-        <div className="w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="file">EVADTS Datei</Label>
-          <Input id="file" type="file" accept=".txt" onChange={handleFileChanged} />
-        </div>
-        <LoadingButton size="lg" loading={loading} onClick={submit} disabled={file === null}>
-          Start
-        </LoadingButton>
+        {!result && <>
+          <div className="w-full max-w-md items-center gap-1.5">
+            <Label htmlFor="file">EVADTS Datei</Label>
+            <Input id="file" type="file" accept=".txt" onChange={handleFileChanged} />
+          </div>
+          <LoadingButton size="lg" loading={loading} onClick={submit} disabled={file === null}>
+            Start
+          </LoadingButton>
+        </>}
+        {result && <>
+          <div className="h-full">
+            <div className="flex flex-row justify-between items-center gap-10 my-5">
+              <div className="flex flex-col">
+                <h1 className="text-xl font-medium">Auswertung</h1>
+                <p className="text-sm text-muted-foreground">Seit dem letzten Zurücksetzen der Verkaufsstatistiken</p></div>
+              <Button variant={"secondary"} onClick={() => { setResult(null); setFile(null); }}>Neue Auswertung starten</Button>
+            </div>
+            <Table className="mb-10 w-full">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Zahlungsart</TableHead>
+                  <TableHead className="w-[150px] text-right">Anzahl Verkäufe</TableHead>
+                  <TableHead className="w-[150px] text-right">Umsatz</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result.paymentMethods.map((p) => (
+                  <TableRow key={p.title}>
+                    <TableCell className="font-medium">{p.title}</TableCell>
+                    <TableCell className="text-right">{p.numberOfSales}</TableCell>
+                    <TableCell className="text-right">{formatCurrency(p.valueOfSales)}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <Table className="w-full">
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[50px]">Fach</TableHead>
+                  <TableHead className="text-left">Einzelpreis</TableHead>
+                  <TableHead className="w-[150px] text-right">Anzahl Verkäufe</TableHead>
+                  <TableHead className="w-[150px] text-right">Umsatz</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result.products.map((p) => (
+                  <TableRow key={p.id}>
+                    <TableCell className="font-medium">{p.id}</TableCell>
+                    <TableCell className="text-left">{formatCurrency(p.price)}</TableCell>
+                    <TableCell className="text-right">{p.numberOfSales}</TableCell>
+                    <TableCell className="text-right">{formatCurrency(p.valueOfSales)}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </>}
       </div>
     </>
   )
--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -5,12 +5,28 @@ interface Product {
     valueOfSales: number; 
 }
 
-type Products = Product[];
+interface PaymentMethod {
+  title: string;
+  valueOfSales: number;
+  numberOfSales: number;
+}
 
-export default function parseEvaDtsFile(content: string): Products {
+type Products = Product[];
+type PaymentMethods = PaymentMethod[];
+export type ParseResult = {
+  products: Products;
+  paymentMethods: PaymentMethods;
+  currencyNumber: number;
+};
+
+export default function parseEvaDtsFile(content: string): ParseResult {
+  let currencyNumber = 978; // EUR
   const products: Products = [];
+  const paymentMethods: PaymentMethods = [];
   const lines = content.split("\n");
   let currentProduct: Product = null!;
+  let currentPaymentMethod: PaymentMethod = null!;
+
 
   for (let line of lines) {
     if (line.trim() === "") continue; // Skip empty lines
@@ -18,6 +34,10 @@ export default function parseEvaDtsFile(content: string): Products {
     const parts = line.split("*").map((part) => part.trim());
     const recordType = parts[0];
     const data = parts.slice(1);
+
+    if(recordType === "ID4") {
+      currencyNumber = Number(data[1].substring(1));
+    }
 
     if(recordType == "PA1") {
         currentProduct = {
@@ -33,7 +53,27 @@ export default function parseEvaDtsFile(content: string): Products {
         currentProduct.numberOfSales = Number(data[2]);
         currentProduct.valueOfSales = Number(data[3]) / 100;
     }
+
+    if(recordType == "CA2") {
+      paymentMethods.push({
+        title: "Barzahlung",
+        numberOfSales: Number(data[3]),
+        valueOfSales: Number(data[2]) / 100,
+      });
+    }
+
+    if (recordType == "DA2") {
+      paymentMethods.push({
+        title: "Kartenzahlung",
+        numberOfSales: Number(data[3]),
+        valueOfSales: Number(data[2]) / 100,
+      });
+    }
   }
 
-  return products;
+  return {
+    products,
+    paymentMethods,
+    currencyNumber
+  };
 }
